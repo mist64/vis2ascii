37ebcddfd4ed555b4dd142463e4daf07c3336ce6
Michael Steil <mist64@mac.com>
2004-09-26 05:30:07 +0200

    initial checkin

---
+++ vis2ascii.c
@@ -0,0 +1,113 @@
+/*
+ * vis2ascii
+ *   by Michael Steil, 19.-20.09.2004
+ *
+ * converts a Commodore 64 VisAss assembly source file
+ * into ASCII
+ *
+ * Usage:
+ * 1) vis2ascii "test		.src"
+ *    - creates test.asm
+ * 2) vis2ascii "test       .src" test.txt
+ *    - creates test.txt
+ * 3) vis2ascii "text       .src" -
+ *    - prints text to stdout
+ *
+ * Notes:
+ * a) vis2ascii automatically strips the spaces between the
+ *    name and the extension if no target name is given.
+ * b) vis2ascii cannot extract files from .D64 images. Use
+ *    a tool like c1541 to extract the files:
+ *      echo extract | c1541 disk.d64
+ *      for i in *.src; do vis2ascii "$i"; done
+ * c) vis2ascii only converts the Vis-Ass file format into
+ *    ASCII, it does not convert the assembly conventions
+ *    into another format. That is, pseudo-opcodes and
+ *    macros stay in Vis-Ass format, but converting this
+ *    manually should be easy.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DATASIZE 65536
+
+char decode[] = "a11cpxcpyldxldystxstyaaxasrarraxsdcpdopisckillarlaxrlarraslosretopadcandaslbitbcsbeqbccbmibnebplbvsbvcbrkclccldcliclvcmpdecdexdeyeorincinxinyjmpjsrldalsrnoporaphaphpplaplprolrorrtirtssbcsecsedseistataxtaytsxtxatxstya.la.ba.by.br.tx.md.de.ma.st.wa.on.wo.kc";
+
+FILE *f1, *f2;
+
+void cbmprintf(unsigned char *ss) {
+	unsigned char *s; char c;
+	for(s=ss; *s!=0; s++) {
+		if (*s<0x20) {
+			c = *s +0x60;
+		} else {
+			c = *s;
+		}
+		fprintf(f2, "%c", c);
+	}
+	fprintf(f2, "\n");
+}
+
+int main(int argc, char **argv) {
+	unsigned char data[DATASIZE];
+	unsigned char *d;
+	int i;
+	char dest[256];
+	char *s, *t;
+
+	switch(argc) {
+		case 2:
+			strcpy(dest, argv[1]);
+			s = strrchr(dest, (int)'.');
+			if (!s) {
+				printf("Source file name does not end with \".src\".\n");
+				exit(1);
+			}
+			for (t = s-1; t>=dest; t--) {
+				if (*t!=' ') break;
+			}
+			t++;
+			strcpy(t, ".asm");
+			break;
+		case 3:
+			strcpy(dest,argv[2]);
+			break;
+		default:
+			printf("vis2ascii\nUsage: vis2ascii source.src source.txt\n");
+			exit(1);
+	}
+
+	fprintf(stderr, "Converting \"%s\" to ", argv[1]);
+	if (dest[0]=='-') {
+		f2 = stdout;
+		fprintf(stderr, "stdout...");
+	} else {
+		f2 = fopen(dest, "w");
+		fprintf(stderr, "\"%s\"...", dest);
+	}
+
+	f1 = fopen(argv[1], "r");
+	fread(data, 1, DATASIZE, f1);
+	fclose(f1);
+
+	d = data+2;
+	while(d[1]) {
+		//		fprintf(f2, "%02x %02x %02x |||", d[0], d[1], d[2]);
+
+		if (d[2]!=0x55) {
+			if (d[2]<0x48) fprintf(f2, "		");
+			fprintf(f2, " ");
+			for (i=0; i<3; i++) fprintf(f2, "%c", decode[d[2]*3+i]);
+			fprintf(f2, " ");
+		}
+
+		d[d[1]]= 0;
+		cbmprintf(&d[3]);
+
+		d+=d[1];
+	}
+	fclose(f2);
+	fprintf(stderr,"done.\n");
+}
